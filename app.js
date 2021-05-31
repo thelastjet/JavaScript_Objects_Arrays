8c9be4a099d7e7925a1d6ebba5d3ae616563ea04
Jay Thomas <thelastjet17@gmail.com>
2021-05-31 13:54:23 -0700

    completed exercises 1 and 2

--- app.js
+++ app.js
@@ -3,7 +3,25 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+function arraySum(numbers) {
+	let sum = 0;
+	numbers.forEach((number) => {
+	sum += number;
+	});
+	return sum;
+}
+console.log(arraySum(numbers));
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+const book = {};
+book.title = "Awaken the Giant Within by Anthony Robbins"
+book.pages = 544;
+book.readCount = 1;
+book.info = function () {
+	return this.title + ", " + this.pages + " pages," + " read " + this.readCount + " time.";
+};
 
+console.log(book.info());
